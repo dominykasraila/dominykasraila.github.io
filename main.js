f791ca0f8a42338cb287d84601053042660d72e3
dominykas.raila <dominykas.raila@gmail.com>
2019-05-22 00:09:41 +0300

    Merge.

--- main.js
+++ main.js
@@ -28,9 +28,16 @@ function showNote(note) {
 
     context.clear();
     var staveNotes = [
-        new VF.StaveNote({ clef: "treble", keys: [note], duration: "q" })
+        new VF.StaveNote({
+            clef: "treble",
+            keys: [note],
+            duration: "q"
+        })
     ];
-    voice = new VF.Voice({ num_beats: 1, beat_value: 4 });
+    voice = new VF.Voice({
+        num_beats: 1,
+        beat_value: 4
+    });
     voice.addTickables(staveNotes);
     var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 100);
     stave.draw();
@@ -48,8 +55,26 @@ function randomArrayElement(arr) {
 
 function keyboardPress(e) {
     var noteName = e.getAttribute("id");
-    var notes = { c: 'c/4', d: 'd/4', e: 'e/4', f: 'f/4', g: 'g/4', a: 'a/4', b: 'b/4' };
+    var notes = {
+        c: 'c/4',
+        d: 'd/4',
+        e: 'e/4',
+        f: 'f/4',
+        g: 'g/4',
+        a: 'a/4',
+        b: 'b/4'
+    };
     if (Object.keys(notes).includes(noteName)) {
-        showNote(notes[noteName]);
+        checkNote(notes[noteName]);
+    }
+}
+
+function checkNote(note) {
+    if (note[0] == currentNote[0]) {
+        currentNote = randomNoteNoRepeat();
+        showNote(currentNote);
+        document.getElementById("correctGuesses").innerHTML = ++correctGuesses;
+    } else {
+        document.getElementById("incorrectGuesses").innerHTML = ++incorrectGuesses;
     }
 }
